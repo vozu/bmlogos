2ac4e1850cb4fce3ca4c87effd000a8f123282b3
superparapluie <39241032+superparapluie@users.noreply.github.com>
2018-05-23 14:38:51 +0200

    Band search
    Ajout band search en jquery

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
-var $grid = $('.w3-row').imagesLoaded( function() {
+var $grid = $('.w3-row').imagesLoaded( function sosotope() {
   $grid.isotope({
     itemSelector: '.grid-item',
     percentPosition: true,
     masonry: {
       columnWidth: '.grid-sizer'
     }
+  });
+});
+
+$(document).ready(function(){
+  $("#searchInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#logoGrid div").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	  var $grid = $('.w3-row').imagesLoaded( function sosotope() {
+  $grid.isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer'
+    }
+  });
+});
+    });
   });
 });
